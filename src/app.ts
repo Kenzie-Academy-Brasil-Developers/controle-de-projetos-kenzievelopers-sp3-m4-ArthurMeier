078e82e9f9a6104818de55aa689933d3536bb893
Arthur Meier <Arthurcmeier@outlook.com>
2023-04-12 17:31:30 -0300

    adicionadas as tabelas do sql e arrumado o problema da port em server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
-import express, { Application } from "express";
+import express, { Application, json } from "express";
 import "dotenv/config";
 
 const app: Application = express();
+
+app.use(json());
+
+app.post("/developers");
+app.get("/developers/:id");
+app.patch("/developers/:id");
+app.delete("/developers/:id");
+app.post("/developers/:id/infos");
 
 export default app;
